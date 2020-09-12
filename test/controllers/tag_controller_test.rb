4233ae8eedb7180d57fae7df17cba603a3f205c2
Franz <Franz@LAPTOP-JC7PJSNR>
2020-09-12 12:38:10 +0800

    bootstraped login form

--- test/controllers/tag_controller_test.rb
+++ test/controllers/tag_controller_test.rb
@@ -8,17 +8,17 @@ class TagsControllerTest < ActionDispatch::IntegrationTest
         sign_in_as(@user)
     end
 
-    test "should get index" do
+    test "7. should get index" do
         get tags_url
         assert_response :success
     end
 
-    test "should get new" do
+    test "8. should get new" do
         get new_tag_url
         assert_response :success
     end
 
-    test "should create Tag" do
+    test "9. should create Tag" do
         post tags_url, params: {tag: { tag_name: "tag name"} } 
         assert_response :redirect
     end
--- test/models/quotation_tag_test.rb
+++ test/models/quotation_tag_test.rb
@@ -14,17 +14,17 @@ class QuotationTagTest < ActiveSupport::TestCase
     @qt = QuotationTag.new(tag_id: @tag.id, quotation_id: @quotation.id)
   end
 
-  test "should not save Quotation Tag without tag id" do
+  test "11. should not save Quotation Tag without tag id" do
     @qt.tag_id = nil
     assert_not @qt.save, "Saved the Quotation Tag without tag id"
   end
 
-  test "should not save Quotation Tag without quotation id" do
+  test "12. should not save Quotation Tag without quotation id" do
     @qt.quotation_id = nil
     assert_not @qt.save, "Saved the Quotation Tag without quotation id"
   end
 
-  test "should save Quotation Tag" do
+  test "13. should save Quotation Tag" do
     assert @qt.save
   end
 
